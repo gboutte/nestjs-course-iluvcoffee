8b0cc62bb995e60eff9219db7d6c9de9cdf35982
gboutte <btte.gregory@gmail.com>
2022-06-20 22:02:34 +0200

    fix indexes

--- src/events/entities/event.entity.ts
+++ src/events/entities/event.entity.ts
@@ -1,6 +1,6 @@
 import { Column, Entity, Index, PrimaryGeneratedColumn } from "typeorm";
 
-@Index('name','type')
+@Index(['name','type'])
 @Entity()
 export class Event {
   @PrimaryGeneratedColumn()
